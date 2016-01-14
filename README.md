9286eb9f04e1217dbaf7f7a278da84503807b24f
strucoder <zdwloveschina@163.com>
2016-01-14 21:37:06 +0800

    update readme.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 介绍
 ----
-`不依赖任何第三方库`  
+`不依赖任何第三方库`
 
 
 如何使用
@@ -11,63 +11,62 @@
 **客户端**
 ```javascript
 var options = {
-	//must
-	domain: '七牛分配给你的域名', 
+  //must
+  domain: '七牛分配给你的域名',
 
-	//must
-	tokenUrl: '你的服务器上获取uptoken的访问地址', 
-	maxImgSize: '最大图片大小',  //option
+  //must
+  tokenUrl: '你的服务器上获取uptoken的访问地址',
+  maxImgSize: '最大图片大小',  //option
 
-	// option , 比如: 'font-qn-uper/'. 访问地址就是:http:domain/prefix/filename
-	prefix: '自定义的路径',
+  // option , 比如: 'font-qn-uper/'. 访问地址就是:http:domain/prefix/filename
+  prefix: '自定义的路径',
 
-	//option, 默认5000ms
-	timeout: '请求超时时间' ,
+  //option, 默认5000ms
+  timeout: '请求超时时间' ,
 
-	//option, 默认480px
-	maxMobileWidth: '在手机上最大显示宽度',
+  //option, 默认480px
+  maxMobileWidth: '在手机上最大显示宽度',
 
-	//option, 默认960px
-	maxPcWidth: '在pc端最大显示宽度',
+  //option, 默认960px
+  maxPcWidth: '在pc端最大显示宽度',
 
-	//option, 格式比如: "jpg, png, jpeg"
-	imageType: "文件类型"
+  //option, 格式比如: "jpg, png, jpeg"
+  imageType: "文件类型"
 }
 var ins = new fontQnUper(options);
 $('#upload').on('change', function() {
-	var file = this.files[0];
+  var file = this.files[0];
 
-	// 错误捕捉
-	ins.errHandle = function(e) {
-		console.log(e);
-	}
+  // 错误捕捉
+  ins.errHandle = function(e) {
+    console.log(e);
+  }
 
-	ins.post(file, function(result) {
-		/*result datas
-			{
-				hash: "", 
-				key: "", 
-				fullImageUrl: "完整的上传图片",
-				mobileImageUrl:"手机上显示的图片",
-				pcImageUrl:"pc端显示的图片"
-			}
-		*/
-	});
-	// 上传进度, total: 一共上传的数据, loaded: 已经上传的数据(bytes)
-	ins.progress = function(total,loaded){
-         //do something 
+  ins.post(file, function(result) {
+    /*result datas
+      {
+        hash: "",
+        key: "",
+        fullImageUrl: "完整的上传图片",
+        mobileImageUrl:"手机上显示的图片",
+        pcImageUrl:"pc端显示的图片"
+      }
+    */
+  });
+  // 上传进度, total: 一共上传的数据, loaded: 已经上传的数据(bytes)
+  ins.progress = function(total,loaded){
+         //do something
     }
 });
 ```
 
-**服务器**
+案例
+====
 
-**注意事项**
-本人使用的是基于`Meteor`的服务端, 如果你换为Nodejs, 可以改一下配置
--  比如获取json文件的配置  
--  路由配置的修改
+我自己的基于`Ghost`的博客系统的前端上传图片
+https://github.com/DgTechOrg/Customize-Ghost
 
 **配置文件说明**
 
->  三个参数, 分别是: `BUCKET_NAME`,	`ACCESS_KEY`, `SECRET_KEY`
+>  三个参数, 分别是: `BUCKET_NAME`,  `ACCESS_KEY`, `SECRET_KEY`
 就是你定义的空间以及七牛提供给你的访问秘钥
--- front/js/uploader.js
+++ front/js/uploader.js
@@ -1,157 +1,157 @@
 (function() {
-	var STR_RAND = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	var DEFAULT_IMG = ['jpg', 'jpeg', 'gif', 'png', 'bmp'];
-	var DEFAULT_SIZE = 10 * 1024;	// 10MB
+  var STR_RAND = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var DEFAULT_IMG = ['jpg', 'jpeg', 'gif', 'png', 'bmp'];
+  var DEFAULT_SIZE = 10 * 1024; // 10MB
 
-	var genRand = function(len) {
-		len = len || 16;
-		var resultStr = '';
-		var length = STR_RAND.length;
-		var random = null;
-		for (var i = 0; i < len; i++) {
-			random = Math.floor(Math.random() * length);
-			resultStr += STR_RAND.substring(random - 1, random);
-		}
-		return resultStr;
-	}
+  var genRand = function(len) {
+    len = len || 16;
+    var resultStr = '';
+    var length = STR_RAND.length;
+    var random = null;
+    for (var i = 0; i < len; i++) {
+      random = Math.floor(Math.random() * length);
+      resultStr += STR_RAND.substring(random - 1, random);
+    }
+    return resultStr;
+  }
 
-	var createAjax = function() {
-		var xmlhttp = {};
-		if (window.XMLHttpRequest) {
-			xmlhttp = new XMLHttpRequest();
-		} else {
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.timeout = this.timeout;
-		return xmlhttp;
-	}
+  var createAjax = function() {
+    var xmlhttp = {};
+    if (window.XMLHttpRequest) {
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.timeout = this.timeout;
+    return xmlhttp;
+  }
 
-	var request = function(options, cb) {
-		var self = this;
-		var xhr = createAjax();
-		xhr.open(options.method, options.url, true);
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-				try {
-					var jsonResult = JSON.parse(xhr.responseText);
-				} catch(e) {
-					e.msg = 'parse text to json error';
-					cb(e);
-				}
-				cb(null, jsonResult);
-			}
-		}
+  var request = function(options, cb) {
+    var self = this;
+    var xhr = createAjax();
+    xhr.open(options.method, options.url, true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        try {
+          var jsonResult = JSON.parse(xhr.responseText);
+        } catch (e) {
+          e.msg = 'parse text to json error';
+          cb(e);
+        }
+        cb(null, jsonResult);
+      }
+    }
 
-		xhr.ontimeout = function() {
-			cb('timeout');
-		}
+    xhr.ontimeout = function() {
+      cb('timeout');
+    }
 
-		xhr.onerror = function(e) {
-			e.msg = 'network error not frontQnUper self error';
-			cb(e);
-		}
-		if (options.onProgress) {
-			xhr.upload.onprogress = function(e) {
-				self.progress(e.total, e.loaded);
-			}
-		}
-		xhr.send(options.method === 'POST' ? options.data : null);
-	}
+    xhr.onerror = function(e) {
+      e.msg = 'network error not frontQnUper self error';
+      cb(e);
+    }
+    if (options.onProgress) {
+      xhr.upload.onprogress = function(e) {
+        self.progress(e.total, e.loaded);
+      }
+    }
+    xhr.send(options.method === 'POST' ? options.data : null);
+  }
 
-	var qnUpload = function(options) {
-		if (Object.prototype.toString.call(options) !== '[object Object]') {
-			throw new Error('options must be Object')
-		}
-		if (options.imageType) {
-			this.imageTypeArr = options.imageType.split(',');
-		} else {
-			this.imageTypeArr = DEFAULT_IMG;
-		}
+  var qnUpload = function(options) {
+    if (Object.prototype.toString.call(options) !== '[object Object]') {
+      throw new Error('options must be Object')
+    }
+    if (options.imageType) {
+      this.imageTypeArr = options.imageType.split(',');
+    } else {
+      this.imageTypeArr = DEFAULT_IMG;
+    }
 
-		this.domain = options.domain; //must
-		this.url = options.tokenUrl; // must
-		this.prefix = options.prefix || 'font-qn-uper/';
-		this.timeout = options.timeout || 5000; // By default 5000ms
-		this.maxMobileWidth = options.maxWidth || 480;
-		this.maxPcWidth = options.maxPcWidth || 960;
-		this.defaultSize = parseInt(options.maxImgSize, 10)  || 10 * 1024;
-		this.uploadUrl = 'http://upload.qiniu.com/?token=';
-		// this._getToken()
-	}
+    this.domain = options.domain; //must
+    this.url = options.tokenUrl; // must
+    this.prefix = options.prefix || 'font-qn-uper/';
+    this.timeout = options.timeout || 5000; // By default 5000ms
+    this.maxMobileWidth = options.maxWidth || 480;
+    this.maxPcWidth = options.maxPcWidth || 960;
+    this.defaultSize = parseInt(options.maxImgSize, 10) || 10 * 1024;
+    this.uploadUrl = 'http://upload.qiniu.com/?token=';
+    // this._getToken()
+  }
 
-	qnUpload.prototype.checkType = function(cb) {
-		var errInfo;
-		var fileName = this.file.name;
-		var fileSize = this.file.size / 1024;
-		var imgExt = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
-		if (this.imageTypeArr.indexOf(imgExt) === -1) {
-			errInfo = '图片格式为下面的一种:  ' + this.imageTypeArr.toString();
-			cb(errInfo);
-		} else if(fileSize > this.defaultSize) {
-			errInfo = '图片大小因控制在'+ this.defaultSize + ' 内';
-			cb(errInfo);
-		} else {
-			cb();
-		}
-	};
+  qnUpload.prototype.checkType = function(cb) {
+    var errInfo;
+    var fileName = this.file.name;
+    var fileSize = this.file.size / 1024;
+    var imgExt = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
+    if (this.imageTypeArr.indexOf(imgExt) === -1) {
+      errInfo = '图片格式为下面的一种:  ' + this.imageTypeArr.toString();
+      cb(errInfo);
+    } else if (fileSize > this.defaultSize) {
+      errInfo = '图片大小因控制在' + this.defaultSize + ' 内';
+      cb(errInfo);
+    } else {
+      cb();
+    }
+  };
 
-	qnUpload.prototype._getToken = function(cb) {
-		var self = this;
-		var options = {
-			method: 'GET',
-			url: this.url
-		}
-		if (typeof cb === 'function') {
-			request(options, cb);
-		} else {
-			throw new Error('when get uptoken the cb must be Function');
-		}
-	}
+  qnUpload.prototype._getToken = function(cb) {
+    var self = this;
+    var options = {
+      method: 'GET',
+      url: this.url
+    }
+    if (typeof cb === 'function') {
+      request(options, cb);
+    } else {
+      throw new Error('when get uptoken the cb must be Function');
+    }
+  }
 
-	qnUpload.prototype._postPrepare = function(cb) {
-		var self = this;
-		this.checkType(function(err) {
-			if (err && typeof self.errHandle === 'function') {
-				return self.errHandle(err);
-			}
-			self._getToken(function(err, tokenInfo) {
-				if (err && typeof self.errHandle === 'function') {
-					return self.errHandle(e);
-				}
-				self.token = tokenInfo;
-				if (typeof cb === 'function') {
-					cb();
-				}
-			});
-		});
-	}
+  qnUpload.prototype._postPrepare = function(cb) {
+    var self = this;
+    this.checkType(function(err) {
+      if (err && typeof self.errHandle === 'function') {
+        return self.errHandle(err);
+      }
+      self._getToken(function(err, tokenInfo) {
+        if (err && typeof self.errHandle === 'function') {
+          return self.errHandle(e);
+        }
+        self.token = tokenInfo;
+        if (typeof cb === 'function') {
+          cb();
+        }
+      });
+    });
+  }
 
-	qnUpload.prototype.post = function(file, cb) {
-		this.file = file;
-		var self = this;
-		var randName = genRand();
-		this._postPrepare(function() {
-			var data = new FormData();
-			data.append('file', file);
-			data.append('key', self.prefix + randName);
-			var postUrl = self.uploadUrl + self.token.uptoken + '&rand=' + Math.random(); // avoid cache
-			var options = {
-				method: 'POST',
-				url: postUrl,
-				onProgress: true,
-				data: data
-			}
-			request.call(self, options, function(err, returnObj) {
-				if (err && typeof errHandle === 'function') {
-					return errHandle(err);
-				}
-				var imageUrlInfo = {};
-				imageUrlInfo.fullImageUrl = self.domain + '/' + returnObj.key;
-				imageUrlInfo.mobileImageUrl = self.domain + '/' + returnObj.key +'?imageView2/0/w/' + self.maxMobileWidth
-				imageUrlInfo.pcImageUrl = self.domain + '/' + returnObj.key + '?imageView2/2/w/' + self.maxPcWidth;
-				cb(imageUrlInfo);
-			});
-		});
-	}
-	window.frontQnUper = qnUpload;
+  qnUpload.prototype.post = function(file, cb) {
+    this.file = file;
+    var self = this;
+    var randName = genRand();
+    this._postPrepare(function() {
+      var data = new FormData();
+      data.append('file', file);
+      data.append('key', self.prefix + randName);
+      var postUrl = self.uploadUrl + self.token.uptoken + '&rand=' + Math.random(); // avoid cache
+      var options = {
+        method: 'POST',
+        url: postUrl,
+        onProgress: true,
+        data: data
+      }
+      request.call(self, options, function(err, returnObj) {
+        if (err && typeof errHandle === 'function') {
+          return errHandle(err);
+        }
+        var imageUrlInfo = {};
+        imageUrlInfo.fullImageUrl = self.domain + '/' + returnObj.key;
+        imageUrlInfo.mobileImageUrl = self.domain + '/' + returnObj.key + '?imageView2/0/w/' + self.maxMobileWidth
+        imageUrlInfo.pcImageUrl = self.domain + '/' + returnObj.key + '?imageView2/2/w/' + self.maxPcWidth;
+        cb(imageUrlInfo);
+      });
+    });
+  }
+  window.frontQnUper = qnUpload;
 })();
